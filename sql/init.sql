df92fb787d086c1ef3c52a33ad66e39c7af5b80a
Rozelin <rozelin.dc@gmail.com>
2022-12-16 12:12:19 +0900

    Update init.sql

--- sql/init.sql
+++ sql/init.sql
@@ -22,18 +22,19 @@ CREATE TABLE `user_deletes` (
 DROP TABLE IF EXISTS `chats`;
 CREATE TABLE `chats` (
   `chat_id` VARCHAR(37) NOT NULL,
+  `room_id` VARCHAR(37) NOT NULL, -- 全体チャットは0?
   `post` VARCHAR(100) NOT NULL,
   `postuser_id`  VARCHAR(37) NOT NULL,
   `created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`chat_id`),
 );
 
--- db for chat owner
-DROP TABLE IF EXISTS `chat_ownerships`;
-CREATE TABLE `chat_ownerships` (
-  `chat_id` VARCHAR(37) NOT NULL,
+-- db for room data
+DROP TABLE IF EXISTS `room_data`;
+CREATE TABLE `room_data` (
+  `room_id` VARCHAR(37) NOT NULL,
   `user_id` VARCHAR(37) NOT NULL,
-  PRIMARY KEY (`chat_id`),
+  PRIMARY KEY (`room_id`, `user_id`),
 );
 
 -- db for session info
