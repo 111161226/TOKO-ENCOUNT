801d6fbe1b5f0f0fdd613500133678364184008e
Rozelin <rozelin.dc@gmail.com>
2022-12-16 12:48:26 +0900

    add destination_user_id

--- db/model/chat.go
+++ db/model/chat.go
@@ -3,21 +3,22 @@ package model
 import "time"
 
 //struct for receiving posted chat talk
-type ChatSimple struct{
-	Post  string `json:"post" db:"post"`
+type ChatSimple struct {
+	Post string `json:"post" db:"post"`
 }
 
 //struct for return posted talk to openchat
-type Chat struct{
-	Post       string `json:"post" db:"post"`
-	ChatId     string `json:"chatId" db:"chat_id"`
-	UserId     string `json:"userId" db:"postuser_id"`
-	UserName   string `json:"userName"`
-	CreatedAt time.Time `json:"createdAt" db:"created_at"`
+type Chat struct {
+	Post              string    `json:"post" db:"post"`
+	ChatId            string    `json:"chatId" db:"chat_id"`
+	UserId            string    `json:"userId" db:"post_user_id"`
+	DestinationUserId string    `json:"destinationUserId" db:"destination_user_id"`
+	UserName          string    `json:"userName" db:"user_name"`
+	CreatedAt         time.Time `json:"createdAt" db:"created_at"`
 }
 
 //struct for private chat talks
 type ChatList struct {
-	HasNext bool           `json:"hasNext"`
-	Pchats   *[]*Chat      `json:"chats"`
+	HasNext bool     `json:"hasNext"`
+	Chats   *[]*Chat `json:"chats"`
 }
