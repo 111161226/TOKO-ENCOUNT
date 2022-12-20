9811d352f5ed56f8e3093d5841d96b219f301666
111161226 <fanrin9@gmail.com>
2022-12-20 11:07:48 +0900

    add getchatlist

--- db/repository/chat.go
+++ db/repository/chat.go
@@ -6,4 +6,5 @@ type ChatRepository interface {
 	PostChat(chatId string, destinationId string, message *model.MessageSimple, post_user_id string, limit int, offset int) (*model.MessageList, error)
 	GetMessages(chatId string, limit int, offset int) (*model.MessageList, error)
 	CreateChat(destinationId string, post_user_id string) (*model.MessageList, error)
+	GetChatList(userId string, limit int offset int) (*model.ChatList, error)
 }
