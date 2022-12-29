65e6b7a0b062abea5aeca6d9dbfb30049c1e8329
koh <kohbanye@gmail.com>
2022-12-30 00:43:15 +0900

    add icons

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,6 +3,7 @@ import './style.scss'
 import App from './App.vue'
 
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import ja from 'element-plus/dist/locale/ja.mjs'
 import 'element-plus/dist/index.css'
 import { createPinia } from 'pinia'
@@ -12,5 +13,8 @@ import router from '@/router'
 const app = createApp(App)
 app.use(router)
 app.use(ElementPlus, { locale: ja })
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(createPinia())
 app.mount('#app')
