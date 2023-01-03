d1081e2b649823efeaa561c2cef923262b520e48
koh <kohbanye@gmail.com>
2023-01-03 16:56:43 +0900

    refactor: split into component

---
+++ client/src/pages/Chat/components/MessageList.vue
@@ -0,0 +1,49 @@
+<script lang="ts" setup>
+import { Message } from '@/lib/apis'
+
+defineProps<{
+  messages: Message[] | undefined
+  myUserName: string | undefined
+}>()
+</script>
+
+<template>
+  <div class="message-list">
+    <div
+      v-for="message in messages"
+      :key="message.chatId"
+      :class="{
+        message: true,
+        'my-message': message.userName === myUserName,
+        'other-message': message.userName !== myUserName
+      }"
+    >
+      {{ message.post }}
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.message-list {
+  display: flex;
+  flex-direction: column;
+}
+.message {
+  white-space: pre-wrap;
+  padding: 0.5rem 1rem;
+  margin: 0.5rem 0;
+}
+.my-message {
+  align-self: flex-end;
+  background-color: $color-primary;
+  color: white;
+  border-radius: 8px;
+  border-bottom-right-radius: 0;
+}
+.other-message {
+  align-self: flex-start;
+  background-color: $bgcolor-primary;
+  border-radius: 8px;
+  border-bottom-left-radius: 0;
+}
+</style>
--- client/src/pages/Chat/index.vue
+++ client/src/pages/Chat/index.vue
@@ -36,19 +36,7 @@ onUpdated(() => {
       {{ otherUserName }}
     </div>
     <div class="content" ref="contentDivRef">
-      <div class="message-list">
-        <div
-          v-for="message in messages"
-          :key="message.chatId"
-          :class="{
-            message: true,
-            'my-message': message.userName === myUserName,
-            'other-message': message.userName !== myUserName
-          }"
-        >
-          {{ message.post }}
-        </div>
-      </div>
+      <message-list :messages="messages" :my-user-name="myUserName" />
     </div>
     <div class="input-container">
       <chat-input class="input" />
@@ -72,29 +60,6 @@ onUpdated(() => {
 .content {
   overflow-y: auto;
   padding: 0.5rem 1rem;
-}
-
-.message-list {
-  display: flex;
-  flex-direction: column;
-}
-.message {
-  white-space: pre-wrap;
-  padding: 0.5rem 1rem;
-  margin: 0.5rem 0;
-}
-.my-message {
-  align-self: flex-end;
-  background-color: $color-primary;
-  color: white;
-  border-radius: 8px;
-  border-bottom-right-radius: 0;
-}
-.other-message {
-  align-self: flex-start;
-  background-color: $bgcolor-primary;
-  border-radius: 8px;
-  border-bottom-left-radius: 0;
 }
 
 .input-container {
