4d5e68f9261255d16ebe72fc665f4e626836a140
111161226 <fanrin9@gmail.com>
2023-01-08 23:50:57 +0900

    add user middleware

---
+++ middleware/auth.go
@@ -0,0 +1,50 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/cs-sysimpl/SakataKintoki/handler"
+	"github.com/labstack/echo/v4"
+)
+
+func EnsureAuthorized(h *handler.Handler) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			path := c.Path()
+			method := c.Request().Method
+			if path == "/api/user" && method == "POST" {
+				// アカウント作成は未ログイン状態で行われるので通す
+				return next(c)
+			}
+
+			_, err := h.PickSession(c)
+			if err != nil {
+				return err
+			}
+
+			return next(c)
+		}
+	}
+}
+
+func EnsureAccessRightToAccount(h *handler.Handler) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			sess, err := h.PickSession(c)
+			if err != nil {
+				return err
+			}
+
+			uid := c.Param("uid")
+			if uid == "" {
+				return echo.NewHTTPError(http.StatusBadRequest, "`uid` is required")
+			}
+
+			if uid != sess.UserId {
+				return echo.NewHTTPError(http.StatusForbidden, "cannot access other's account")
+			}
+
+			return next(c)
+		}
+	}
+}
