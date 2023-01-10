12357d8b96d93aacae544878f6d3e471333e0698
111161226 <fanrin9@gmail.com>
2023-01-10 11:49:08 +0900

    remove uid process

--- middleware/auth.go
+++ middleware/auth.go
@@ -26,25 +26,3 @@ func EnsureAuthorized(h *handler.Handler) echo.MiddlewareFunc {
 		}
 	}
 }
-
-func EnsureAccessRightToAccount(h *handler.Handler) echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(c echo.Context) error {
-			sess, err := h.PickSession(c)
-			if err != nil {
-				return err
-			}
-
-			uid := c.Param("uid")
-			if uid == "" {
-				return echo.NewHTTPError(http.StatusBadRequest, "`uid` is required")
-			}
-
-			if uid != sess.UserId {
-				return echo.NewHTTPError(http.StatusForbidden, "cannot access other's account")
-			}
-
-			return next(c)
-		}
-	}
-}
