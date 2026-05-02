09465d66a5bc3678c0ef379d7872cc3d13aa6207
111161226 <fanrin9@gmail.com>
2026-05-03 00:48:44 +0900

    fix vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,31 +1,35 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite' // loadEnv を追加
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-const devHost = import.meta.env.VITE_API_HOST 
+export default defineConfig(({ mode }) => {
+  // 現在の作業ディレクトリにある環境変数を読み込む
+  // 第3引数を '' にすると、VITE_ 以外の変数も読み込めます
+  const env = loadEnv(mode, process.cwd(), '');
+  const devHost = env.VITE_API_HOST || 'localhost:3050';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src').replace(/\\/g, '/')
-    }
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: `https://${devHost}`,
-        changeOrigin: true
+  return {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src').replace(/\\/g, '/')
       }
-    }
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/variables.scss";`
+    },
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: `https://${devHost}`,
+          changeOrigin: true
+        }
       }
-    }
-  },
-  plugins: [vue()]
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "@/variables.scss";`
+        }
+      }
+    },
+    plugins: [vue()]
+  }
 })
